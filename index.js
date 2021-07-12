61d909367a573cd51f16a2b012ea2ae30bd37172
Chanel Donahoe <cdonahoe05@gmail.com>
2021-07-12 23:38:50 +1000

    completed tasks

---
+++ index.js
@@ -0,0 +1,31 @@
+// let name = "Josh"
+
+function introduction(name) {
+   return (`Hi, my name is ${name}.`);
+}
+
+
+
+// let name = "Nancy"
+// let language = "React"
+// function introductionWithLanguage(name, language) {
+//     return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+// }
+
+// let language = Ember.js
+
+function introductionWithLanguage(name, language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+// let name = "Gracie"
+// let language = "Python"
+
+// function introductionWithLanguageOptional(name, language = "Javascript") {
+//     return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+// }
+
+// let name = "Gracie"
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
